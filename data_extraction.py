2b85424e260bbb9fa694861bf71ffe892a8c9db9
alessia96 <66359835+alessia96@users.noreply.github.com>
2022-11-12 12:01:19 +0100

    updated data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -8,14 +8,14 @@ from facelib import FaceDetector, AgeGenderEstimator, EmotionDetector
 from pymongo import MongoClient as Client
 
 
-class StreamCustomerData:
+class CustomerData:
     def __init__(self, collection):
         self.collection = collection
         self.age_gender = AgeGenderEstimator()
         self.emotion = EmotionDetector()
-        
+
     # extract features from images
-    def extract_info(self, img_path):
+    def extract_info(self, img_path, img_time):
         img = cv2.imread(img_path)
         detector = FaceDetector()
         faces, boxes, scores, landmarks = detector.detect_align(img)
@@ -26,7 +26,8 @@ class StreamCustomerData:
         genders, ages = self.age_gender.detect(faces)
         emotions, probab = self.emotion.detect_emotion(faces)
 
-        out = [{"time": img_time, "gender": genders[i], "age": ages[i], "emotion": emotions[i]} for i in range(len(ages))]
+        out = [{"time": img_time, "gender": genders[i], "age": ages[i], "emotion": emotions[i]} for i in
+               range(len(ages))]
 
         return out
 
@@ -40,17 +41,17 @@ class StreamCustomerData:
 if __name__ == "__main__":
     client = Client("mongodb://127.0.0.1/")
     collection = client.shop.customers
+    img_path = "./faces/"
 
     img_done = set()
-    
+
+    C = CustomerData(collection)
     while True:
         imgs_path = set(glob(img_path + "*.jpg"))
         for image in imgs_path:
             if image not in img_done:
-                
-                im = cv2.imread(image)
-                t = image.split() # get timestamp
-                data = C.extract_info(im, t)
-                C.write_to_mongo(data)
-                img_done.add(image)
-                
+                t = image.split("_")[-1].replace("jpg", "")  # get image time
+                data = C.extract_info(image, t)    # extract metadata
+                C.write_to_mongo(data)  # save data into MongoDB collection
+                img_done.add(image)     # add image path to done images
+                print(data)
